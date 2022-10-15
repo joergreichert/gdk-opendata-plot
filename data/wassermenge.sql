2d7555657afc7af2234d6bbceb7bc17844353cac
Jörg Reichert <de.abg.reichert.joerg@googlemail.com>
2022-10-15 22:24:05 +0200

    Update 2022

--- data/wassermenge.sql
+++ data/wassermenge.sql
@@ -7,12 +7,12 @@ select
   t.gattung as gattungdeutsch,
   t.strname,
   t.pflanzjahr,
-  2021-t.pflanzjahr as standalter,
+  2022-t.pflanzjahr as standalter,
   t.bezirk,
   t.standortnr,
   max(w.amount::int) as wassermenge,
   sum(w.amount::int) as wassersumme,
   count(w) as anzahlg
 from trees t join trees_watered w on t.id = w.tree_id
-where w.timestamp >= '20210301' and w.timestamp < '20210930'
+where w.timestamp >= '20220301' and w.timestamp < '20220930'
 group by t.id
